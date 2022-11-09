c8e8540a0514231da144cfafc5c9bba8f402422c
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-11-09 18:36:49 +0100

    Added converted (yet to add logic

--- wage-calculator/src/App.jsx
+++ wage-calculator/src/App.jsx
@@ -13,23 +13,36 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Wage Converter</h1>
-        <div className="wage-input">
-          <label htmlFor="thirtyfiveWage">35 Hour Wage</label>
-          <input
-            type="number"
-            id="thirtyfiveWage"
-            value={thirtyfiveWage}
-            onChange={(e) => setThirtyfiveWage(e.target.value)}
-          />
-        </div>
-        <div className="wage-input">
-          <label htmlFor="thirtyNineWage">39 Hour Wage</label>
-          <input
-            type="number"
-            id="thirtyNineWage"
-            value={thirtyNineWage}
-            onChange={(e) => setThirtyNineWage(e.target.value)}
-          />
+        <div className="wage-calculator">
+          <div className="wage-calculator-row">
+            <div className="wage-input">
+              <label htmlFor="thirtyfiveWage">35 Hour Wage : </label>
+              <input
+                type="number"
+                id="thirtyfiveWage"
+                value={thirtyfiveWage}
+                onChange={(e) => setThirtyfiveWage(e.target.value)}
+              />
+            </div>
+            <div className="wage-calculated">
+              This makes {convertedThirtyfiveWage} for 39 hours.
+            </div>
+          </div>
+
+          <div className="wage-calculator-row">
+            <div className="wage-input">
+              <label htmlFor="thirtyNineWage">39 Hour Wage : </label>
+              <input
+                type="number"
+                id="thirtyNineWage"
+                value={thirtyNineWage}
+                onChange={(e) => setThirtyNineWage(e.target.value)}
+              />
+            </div>
+            <div className="wage-calculated">
+              This makes {convertedThirtyfiveWage} for 35 hours.
+            </div>
+          </div>
         </div>
       </header>
     </div>
